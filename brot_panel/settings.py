3e73af3ac5549490c29759ac06020b8d0f972342
Matias <lopezmaty@gmail.com>
2026-07-04 11:51:37 -0300

    super user and urls setup

--- brot_panel/settings.py
+++ brot_panel/settings.py
@@ -91,6 +91,12 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
