925693980728f76cabdd2cef18b1e56429f4e332
Matias <lopezmaty@gmail.com>
2026-07-05 11:06:25 -0300

    permissions assigned to views

--- lista_precios/views.py
+++ lista_precios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from . import serializers, models
+from users.permissions import EsAdmin, EsColab, EsLector
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -8,26 +10,61 @@ class TipoClienteViewset(viewsets.ModelViewSet):
     queryset = models.TipoCliente.objects.all()
     serializer_class = serializers.TipoClienteSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
+
 class VariedadViewset(viewsets.ModelViewSet):
     queryset = models.Variedad.objects.all()
     serializer_class = serializers.VariedadSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]    
 
 class TamañoViewset(viewsets.ModelViewSet):
     queryset = models.Tamaño.objects.all()
     serializer_class = serializers.TamañoSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
+
 class FamiliaViewset(viewsets.ModelViewSet):
     queryset = models.Familia.objects.all()
     serializer_class = serializers.FamiliaSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
 
 class ProductoViewset(viewsets.ModelViewSet):
     queryset = models.Producto.objects.all()
     serializer_class = serializers.ProductoSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
+
 class ListaPreciosViewset(viewsets.ModelViewSet):
     queryset = models.ListaPrecios.objects.all()
     serializer_class = serializers.ListaPrecioSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
+
 class PreciosViewset(viewsets.ModelViewSet):
     queryset = models.Precio.objects.all()
     serializer_class = serializers.PrecioSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        return [EsAdmin()]
