b9f6ad2923894f9ca29e73ef26a2015928a9d932
Matias <lopezmaty@gmail.com>
2026-07-18 19:38:59 -0300

    UserSerializer: separar rol (write_only) y rol_actual (read_only con source) para permitir creación vía API

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,22 @@ from . import models
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    rol = serializers.SerializerMethodField()
+    rol = serializers.CharField(write_only=True)
+    rol_actual = serializers.CharField(source='perfil.rol', read_only=True)
 
-    def get_rol(self, obj):
-            return obj.perfil.rol
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'rol']
+        fields = ['id', 'username', 'email', 'rol', 'rol_actual']
+
+    def create(self, validated_data):
+        rol = validated_data.pop('rol')
+        user = User.objects.create_user(**validated_data)
+        user.set_unusable_password()
+        user.save()
+        models.Perfil.objects.create(usuario=user, rol= rol)
+
+        return user
+        
 
 
     
