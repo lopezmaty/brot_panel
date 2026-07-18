a0624af651aac7985d6f94400783ebd539163a33
Matias <lopezmaty@gmail.com>
2026-07-18 19:07:16 -0300

    UserSerializer: create() personalizado (user sin password + Perfil), EMAIL_BACKEND de consola

--- brot_panel/settings.py
+++ brot_panel/settings.py
@@ -141,3 +141,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'no-reply@brotpanes.com'
