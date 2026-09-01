bb6c4228580e43faeba0f63c62e6f11ad325439d
Matias <lopezmaty@gmail.com>
2026-09-01 19:03:45 -0300

    feat: redirigir la raiz del sitio al login del panel

--- brot_panel/urls.py
+++ brot_panel/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from frontend.views import catalogo_view, perfil_catalogo_view
 from sistema_pedidos.views import confirmar_pedido_catalogo
@@ -24,6 +25,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/panel/login/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
